7a0fbd27a002674d5a08278cc1d51d0ba82fc98b
Ankmaniac <mlgisawsome02@gmail.com>
2025-10-18 18:16:29 -0400

    slightly improved shelfrenderer but im not sure if it actually works

--- src/main/java/com/ankmaniac/decofirmacraft/client/render/blockentity/DFCShelfBlockEntityRenderer.java
+++ src/main/java/com/ankmaniac/decofirmacraft/client/render/blockentity/DFCShelfBlockEntityRenderer.java
@@ -3,12 +3,15 @@ package com.ankmaniac.decofirmacraft.client.render.blockentity;
 import com.ankmaniac.decofirmacraft.common.block.DFCShelfBlock;
 import com.ankmaniac.decofirmacraft.common.blockentities.DFCShelfBlockEntity;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import net.dries007.tfc.common.component.size.ItemSizeManager;
 import net.dries007.tfc.common.component.size.Weight;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
@@ -27,6 +30,7 @@ public class DFCShelfBlockEntityRenderer implements BlockEntityRenderer<DFCShelf
             final ItemStack item = shelf.getInventory().getStackInSlot(i);
             if (!item.isEmpty())
             {
+                final ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
                 poseStack.pushPose();
 
                 final Vec3 pos = DFCShelfBlock.SLOT_CENTERS.get(shelf.getBlockState().getValue(DIRECTION)).get(i);
@@ -39,61 +43,40 @@ public class DFCShelfBlockEntityRenderer implements BlockEntityRenderer<DFCShelf
                 float filled = (float) stackSize / (float) maxStackSize;
                 int perBlock = maxStackSize / 8;
                 int stackCount = (int) Math.floor((double) item.getCount() / (double) perBlock);
+                int maxStacks = 8;
+
 
                 switch(itemWeight){
-                    case VERY_LIGHT:
-                        poseStack.scale(0.2f, 0.2f, 0.2f);
-                        break;
-                    case LIGHT:
+                    case VERY_LIGHT, LIGHT, MEDIUM, HEAVY:
                     default:
                         poseStack.scale(0.3f, 0.3f, 0.3f);
-                        break;
-                    case MEDIUM:
-                        poseStack.scale(0.4f, 0.4f, 0.4f);
-                        poseStack.translate(0f, 0.1f, 0f);
-                        break;
-                    case HEAVY:
-                        poseStack.scale(0.45f, 0.45f, 0.45f);
-                        poseStack.translate(0f, 0.1f, 0f);
+                        if (itemWeight == Weight.HEAVY){
+                            maxStacks = 4;
+                        }
                         break;
                     case VERY_HEAVY:
                         poseStack.scale(0.5f, 0.5f, 0.5f);
                         poseStack.translate(0f, 0.15f, 0f);
+                        maxStacks = 1;
                         break;
                 }
 
                 int iteration = 0;
                 for (int j = 0; j < stackSize; j++) {
                     poseStack.pushPose();
-                    switch(itemWeight){
-                        case HEAVY:
-                            if (iteration >= 4){
-                                break;
-                            }
-                            else{
-                                iteration++;
-                            }
-                            poseStack.pushPose();
-                            poseStack.scale(0.45f, 0.45f, 0.45f);
-                            poseStack.translate(0f, 0.1f, 0f);
-                            if (j == stackSize - 1 && j % 2 == 0){
-                                poseStack.translate(0, 0, j > 1 ? .5f : 0);
-                            }
-                            else {
-                                poseStack.translate(j % 2 == 0 ? .5f : -.5f, 0f, j > 1 ? .5f : -.5f);
-                            }
-                            poseStack.popPose();
-                        case VERY_HEAVY:
-                            if (iteration >= 1){
-                                break;
-                            }
-                            else{
-                                iteration++;
-                            }
-                            poseStack.pushPose();
-                            poseStack.scale(0.5f, 0.5f, 0.5f);
-                            poseStack.translate(0f, 0.15f, 0f);
-                            poseStack.popPose();
+                    if (iteration >= maxStacks){
+                        break;
+                    }
+                    else if (iterationCounter(itemWeight, j, perBlock)){
+                        poseStack.popPose();
+                        if (j == stackSize - 1 && j % 2 == 0){
+                            poseStack.translate(0, j > 4 ? .5f : 0f, j > 1 ? .5f : 0);
+                        }
+                        else {
+                            poseStack.translate(j % 2 == 0 ? .5f : -.5f, j > 4 ? .5f : 0f, j > 1 ? .5f : -.5f);
+                        }
+                        poseStack.popPose();
+                        iteration++;
                     }
                     poseStack.popPose();
                 }
@@ -101,6 +84,15 @@ public class DFCShelfBlockEntityRenderer implements BlockEntityRenderer<DFCShelf
                 Minecraft.getInstance().getItemRenderer().renderStatic(item, ItemDisplayContext.FIXED, combinedLight, combinedOverlay, poseStack, buffer, shelf.getLevel(), 0);
                 poseStack.popPose();
             }
+        }
+    }
+    private boolean iterationCounter(Weight weight, int j, int perBlock) {
+        switch (weight) {
+            case VERY_LIGHT, LIGHT, MEDIUM:
+            default:
+                return j % perBlock == 0;
+            case HEAVY, VERY_HEAVY:
+                return true;
         }
     }
 }
